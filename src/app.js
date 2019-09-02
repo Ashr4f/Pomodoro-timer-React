a1dacb47cb93637ca0631de4df008605be95c4ee
Ashraf <ashraf@post.com>
2019-09-02 16:55:06 +0200

    Added ESLint correctly

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
+global.jQuery = require("jquery");
+require("bootstrap");
+import "bootstrap/dist/css/bootstrap.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./app.scss";
 
-const App = () => {
-	return (
-		<div>
-			<h1>Hello world</h1>
-		</div>
-	);
-};
+const App = () => (
+    <div>
+        <h1>{"Hello world"}</h1>
+    </div>
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
