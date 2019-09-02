58b53879cf9eacfc2d3bda98783403cee0303458
Ashraf <ashraf@post.com>
2019-09-02 10:54:20 +0200

    Initialize React with Parcel

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.scss";
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello world</h1>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
