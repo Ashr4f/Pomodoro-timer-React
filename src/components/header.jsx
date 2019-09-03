437baf796437eaf9fd69ff0afdf6be97f24842cc
Ashraf <ashraf@post.com>
2019-09-03 22:40:36 +0200

    Added fork-me from GitHub flag

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function PomodoroTimer() {
-    return <React.Fragment>{"Header"}</React.Fragment>;
+export default function Header() {
+    return (
+        <div className={"header-container"}>
+            <p>{"Pomodoro Timer"}</p>
+            <a
+                className={"github-fork-container"}
+                href={"https://github.com/Ashr4f/Pomodoro-timer-React"}>
+                <img
+                    src={
+                        "https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png"
+                    }
+                    className={"attachment-full size-full github-fork"}
+                    alt={"Fork me on GitHub"}
+                    data-recalc-dims={"1"}
+                />
+            </a>
+        </div>
+    );
 }
